b8fd66e78184d95ddef4490ed3d9bdde56e0b518
tawfiqjaffar <tawfiq.jaffar@epitech.eu>
2021-03-09 12:18:32 +0100

    added more activities and projects

--- lib/data/activities.dart
+++ lib/data/activities.dart
@@ -5,37 +5,37 @@ final List<Activity> cProgActivities = [
       id: 0,
       projectId: 0,
       label: "Introduction",
-      start: DateTime(2021, 3, 6, 12, 30)),
+      start: DateTime(2021, 3, 9, 12, 30)),
   Activity(
       id: 1,
       projectId: 0,
       label: "putchar",
-      start: DateTime(2021, 3, 6, 14, 00)),
+      start: DateTime(2021, 3, 9, 14, 00)),
   Activity(
       id: 2,
       projectId: 0,
       label: "putstrasdfasdf",
-      start: DateTime(2021, 3, 6, 14, 00)),
+      start: DateTime(2021, 3, 9, 14, 00)),
   Activity(
       id: 3,
       projectId: 0,
       label: "strlen",
-      start: DateTime(2021, 3, 7, 14, 00)),
+      start: DateTime(2021, 3, 10, 14, 00)),
   Activity(
       id: 4,
       projectId: 0,
       label: "putnbr",
-      start: DateTime(2021, 3, 7, 14, 00)),
+      start: DateTime(2021, 3, 11, 14, 00)),
   Activity(
       id: 5,
       projectId: 0,
       label: "strcmp",
-      start: DateTime(2021, 3, 7, 14, 00)),
+      start: DateTime(2021, 3, 12, 14, 00)),
   Activity(
       id: 6,
       projectId: 0,
       label: "strncmp",
-      start: DateTime(2021, 3, 7, 14, 00)),
+      start: DateTime(2021, 3, 13, 14, 00)),
 ];
 
 final List<Activity> cPPProgActivities = [
@@ -44,4 +44,27 @@ final List<Activity> cPPProgActivities = [
       projectId: 1,
       label: "Introduction",
       start: DateTime(2021, 3, 15, 14, 00)),
+];
+
+final List<Activity> javaActivities = [
+  Activity(
+      id: 0,
+      projectId: 2,
+      label: "Introduction",
+      start: DateTime(2021, 3, 9, 17, 00)),
+  Activity(
+      id: 0,
+      projectId: 2,
+      label: "Data types and structures",
+      start: DateTime(2021, 3, 9, 18, 00)),
+  Activity(
+      id: 0,
+      projectId: 2,
+      label: "Collections",
+      start: DateTime(2021, 3, 9, 18, 30)),
+  Activity(
+      id: 0,
+      projectId: 2,
+      label: "Streams and Threads",
+      start: DateTime(2021, 3, 9, 19, 00))
 ];
--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -4,5 +4,6 @@ import 'package:Diligent/models/project.dart';
 final List<Project> projects = [
   Project(id: 0, label: "C for beginners", activities: cProgActivities),
   Project(id: 1, label: "C++ for beginners", activities: cPPProgActivities),
-  Project(id: 2, label: "Java Spring boot/Hibernate", activities: [])
+  Project(
+      id: 2, label: "Java Spring boot/Hibernate", activities: javaActivities)
 ];
